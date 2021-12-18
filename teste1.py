69a9f319df39f94a7d515a8f8d83f46d611cc37d
Rodrigo Duarte <rodpydev@gmail.com>
2021-12-18 14:47:16 -0500

    teste

--- teste1.py
+++ teste1.py
@@ -1,7 +1,7 @@
 # multiplicacao
 numero = int(input("informe um numero entre 1 e 10: "))
 
-while numero < 1 or numero >10:
+while numero < 1 or numero > 10:
     numero = int(input("informe um numero entre 1 e 10: "))
 
 print("tabuada de {0}".format(numero))
